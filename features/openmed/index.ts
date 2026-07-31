47eb3d2863c238774ef4285710fa55c2f7463cb2
Yasser <116118149+YrFnS@users.noreply.github.com>
2026-07-31 16:42:34 +0300

    feat: export OpenMed integration module

---
+++ features/openmed/index.ts
@@ -0,0 +1,6 @@
+export * from './candidateMapping';
+export * from './documentText';
+export * from './openMedClient';
+export * from './openMedExtractionService';
+export * from './schemas';
+export * from './types';
